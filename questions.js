35d7a6dda06ad5b72015678ecd4fd119fe5272a9
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-10-01 15:00:41 -0700

    moved questions to separate js file so code is less cluttered

---
+++ questions.js
@@ -0,0 +1,51 @@
+const questions = [{
+    type:"input",
+    message: "What is the title of your project?",
+    name: "title"
+},
+{
+    type:"input",
+    message:"Provide a description",
+    name: "description"
+},
+{
+    type:"input",
+    message:"Provide installation instructions",
+    name: "installation"
+},
+{
+    type:"input",
+    message:"Provide usage information",
+    name:"usage"
+},
+{
+    type:"input",
+    message:"Provide contribution guidelines",
+    name:"contribution"
+},
+{
+    type:"input",
+    message:"Provide test instructions",
+    name:"test"
+},
+{
+    type:"list",
+    message:"What license would you like for your project?",
+    choices:["MIT","Apache 2.0","GNU GPL v3.0"],
+    name:"license"
+},
+{
+    type:"input",
+    message:"Enter your GitHub username",
+    name:"github"
+},
+{
+    type:"input",
+    message:"Enter your email address",
+    name:"email"
+}
+];
+
+module.exports = {
+    questions:questions
+}
